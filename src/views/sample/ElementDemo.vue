8fdcbcaf61d5ade44e7ea3f759031d6399f72842
david-van <fanzunying@gmail.com>
2025-10-25 10:44:37 +0800

    icon自动导入

--- src/views/sample/ElementDemo.vue
+++ src/views/sample/ElementDemo.vue
@@ -1,11 +1,37 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
-<el-button type="primary" size="small">Primary</el-button>
+  <el-button type="primary" size="small">Primary</el-button>
+  <!--  <el-row justify="center">-->
+  <!-- 默认的不行 -->
+  <!-- <el-icon><Female /></el-icon> -->
+  <p>--------1-------------</p>
+  <!-- 配置为 prefix: "icon" 时 -->
+  <el-icon size="20px"><icon-ep-MessageBox /></el-icon>
+  <p>--------2-------------</p>
+  <el-icon><icon-ep-female /></el-icon>
+  <el-icon><icon-ep-add-location /></el-icon>
+  <p>---------3------------</p>
+  <el-icon size="20px"><IconDocumentation /></el-icon>
+  <el-icon size="40px"><IconMylogo /></el-icon>
+  <p>----------4---------------</p>
+  <icon-mdi-arrow-left-top-bold v-bind:style="{ width: '40px', height: '40px' }"  />
+  <!-- prefix: "fct" -->
+  <!--    <el-icon><FctEpApple /></el-icon>-->
+
+  <!-- prefix走默认配置时  -->
+  <!-- <el-icon><i-ep-add-location /></el-icon> -->
+
+  <!-- 禁用prefix配置时, prefix: false  -->
+  <!--    <el-icon><ep-MessageBox /></el-icon>-->
+  <!--    <el-icon><EpSuccessFilled /></el-icon>-->
+  <!--  </el-row>-->
 </template>
 
 <style scoped>
-
+/*el-icon {
+  display: block;
+  margin: 10px 0;
+}*/
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import IconsResolver from 'unplugin-icons/resolver'
+import { FileSystemIconLoader } from 'unplugin-icons/loaders'
+import Icons from 'unplugin-icons/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -18,8 +22,25 @@ export default defineConfig({
       dts: 'src/auto-imports.d.ts',
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({
+          prefix: 'icon',
+          // this is optional, default enabling all the collections supported by Iconify
+          // enabledCollections: ['ep'], // 这是element-plus在iconify的集合名,
+          // 这行不配置也可以, 会识别iconify有的集合名 只要用了就自动下载 自动导入
+          customCollections: ['my'], // **** 这里配置组件内需要使用的自定义集合名
+        }),
+      ],
       dts: 'src/components.d.ts',
+    }),
+    Icons({
+      // experimental
+      compiler: 'vue3',
+      autoInstall: true,
+      customCollections: {
+        my: FileSystemIconLoader('src/assets/icons'), //配置自定义集合的路径, 更详细的配置可参考插件仓库
+      },
     }),
   ],
   resolve: {
